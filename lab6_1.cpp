5dc361002f424a4526ef5cab5c763dbfa7a15047
LAB303-6 <lab303-6@vdieng.local>
2024-12-03 13:34:46 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,24 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+int main() {
+    int number, even = 0, odd = 0;
+    while (true) {
+        cout << "Enter an integer: ";
+        cin >> number;
+
+        if (number == 0) {
+            break;
+        }
+
+        if (number % 2 == 0) {
+            even++;
+        } else {
+            odd++;
+        }
+    }
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd << endl;
+
     return 0;
 }
